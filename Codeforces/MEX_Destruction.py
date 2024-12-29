e1a07c4baf415e8ad36646c5e3448f7a359e26ac
rayane_rousseau <rousseau.rayane@gmail.com>
2024-12-29 22:39:50 +0100

    :memo: 800 elo problem

---
+++ Codeforces/MEX_Destruction.py
@@ -0,0 +1,22 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    while a and a[-1] == 0:
+        a.pop()
+
+    a.reverse()
+    while a and a[-1] == 0:
+        a.pop()
+    a.reverse()
+
+    if not a:
+        print(0)
+        continue
+
+    has_zero = any(x == 0 for x in a)
+    if has_zero:
+        print(2)
+    else:
+        print(1)
