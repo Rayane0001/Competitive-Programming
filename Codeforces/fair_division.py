e8ae7cd0c99f688470aaff6710a0945a5038402a
rayane_rousseau <rousseau.rayane@gmail.com>
2025-01-26 21:22:16 +0100

    :memo 800 elo problem

--- Codeforces/fair_division.py
+++ Codeforces/fair_division.py
@@ -4,15 +4,5 @@ for _ in range(int(input())):
     a = list(map(int, input().split()))
 
     t = Counter(a)
-    nb1 = t[1]
-    nb2 = t[2]
-
-    total = nb1 + nb2 * 2
-    if total % 2 != 0:
-        print("NO")
-    else:
-        half = total // 2
-        if half % 2 == 0 or (half% 2 == 1 and nb1>0):
-            print("YES")
-        else:
-            print("NO")
+    total = t[1] + t[2] * 2
+    print("YES" if total % 2 == 0 and (total // 2 % 2== 0 or t[1] > 0) else "NO")
