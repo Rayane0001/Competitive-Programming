053dbb6c40b61582b63ca17de09921ea1243c9ec
rayane_rousseau <rousseau.rayane@gmail.com>
2025-01-30 23:34:27 +0100

    :memo 900 elo problem

--- Codeforces/make_majority.py
+++ Codeforces/make_majority.py
@@ -6,7 +6,14 @@ res = []
 
 for _ in range(t):
     n, a, i = int(data[i]), data[i+1], i + 2
-    ok = a.count("111") >= 1 or a.count("11") >= 2 or (a.count("11") >= 1 and (a[0] == "1" or a[-1] == "1")) or (a[0] == "1" and a[-1] == "1")
+
+    ok = any([
+        a.count("111") >= 1,
+        a.count("11") >= 2,
+        a.count("11") >= 1 and (a[0] == "1" or a[-1] == "1"),
+        a[0] == "1" and a[-1] == "1"
+    ])
+
     res.append("Yes" if ok else "No")
 
 sys.stdout.write("\n".join(res) + "\n")
