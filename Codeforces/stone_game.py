df61fbbcb42febfeaf1c50de98094dbeaad2c484
rayane_rousseau <rousseau.rayane@gmail.com>
2025-02-01 23:39:33 +0100

    :memo simpler

--- Codeforces/stone_game.py
+++ Codeforces/stone_game.py
@@ -8,14 +8,14 @@ for _ in range(t):
     stones = list(map(int, data[i + 1:i + 1 + n]))
     i += n + 1
 
-    min_idx = stones.index(min(stones))
-    max_idx = stones.index(max(stones))
+    max_pos = stones.index(max(stones))
+    min_pos = stones.index(min(stones))
 
-    option1 = max(min_idx + 1, max_idx + 1)
-    option2 = max(n - min_idx, n - max_idx)
-    option3 = (min_idx + 1) + (n - max_idx)
-    option4 = (max_idx + 1) + (n - min_idx)
+    res = min(
+        max(max_pos, min_pos) + 1,
+        n - min(max_pos, min_pos),
+        (n - max_pos) + (min_pos + 1),
+        (n - min_pos) + (max_pos + 1)
+    )
 
-    result = min(option1, option2, option3, option4)
-
-    sys.stdout.write(f"{result}\n")
+    sys.stdout.write(f"{res}\n")
