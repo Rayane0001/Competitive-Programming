f8a8334666208b92aeb36b7aae9431a9dd63522d
rayane_rousseau <rousseau.rayane@gmail.com>
2025-02-06 16:21:21 +0100

    :memo crosswords.py

--- Jean-Perrin/crosswords.py
+++ Jean-Perrin/crosswords.py
@@ -1,14 +1,48 @@
-n,a=map(int,input().split())
-m,b=map(int,input().split())
-v=[input() for _ in range(a)]
-h=[input() for _ in range(b)]
-S=set(v)
-import itertools
-c=0
-for rows in itertools.product(h,repeat=n):
-    for j in range(m):
-        if "".join(rows[i][j] for i in range(n)) not in S:
-            break
-    else:
-        c+=1
-print(c)
+import sys
+
+def build_prefix_set(words):
+    prefixes = set()
+    for word in words:
+        for i in range(1, len(word) + 1):
+            prefixes.add(word[:i])
+    return prefixes
+
+def count_boards(n, m, vertical_words, horizontal_lines):
+    prefix_set = build_prefix_set(vertical_words)
+    count = 0
+    board = []
+
+    def backtrack(row_index):
+        nonlocal count
+        if row_index == n:
+            for j in range(m):
+                col = ''.join(board[i][j] for i in range(n))
+                if col not in vertical_words:
+                    return
+            count += 1
+            return
+
+        for line in horizontal_lines:
+            valid = True
+            for j in range(m):
+                vertical_prefix = ''.join(board[i][j] for i in range(row_index)) + line[j]
+                if vertical_prefix not in prefix_set:
+                    valid = False
+                    break
+            if valid:
+                board.append(line)
+                backtrack(row_index + 1)
+                board.pop()
+
+    backtrack(0)
+    return count
+
+data = sys.stdin.read().splitlines()
+it = iter(data)
+n, a = map(int, next(it).split())
+m, b = map(int, next(it).split())
+vertical_words = {next(it).strip() for _ in range(a)}
+horizontal_lines = [next(it).strip() for _ in range(b)]
+
+result = count_boards(n, m, vertical_words, horizontal_lines)
+sys.stdout.write(str(result))
