f8a617579f3ec41ac20eb32aa2f95673b5bbbabe
rayane_rousseau <rousseau.rayane@gmail.com>
2025-02-06 15:59:41 +0100

    :memo improvements

--- Jean-Perrin/yet_satisfiability_again.py
+++ Jean-Perrin/yet_satisfiability_again.py
@@ -1,11 +1,11 @@
-import sys, itertools
+import sys
 
 data=sys.stdin.read().strip().splitlines()[1:]
-clauses=[list(map(int, line.split()[:-1])) for line in data]
-v=max(abs(lit) for clause in clauses for lit in clause)
+clauses= [list(map(int,line.split()[:-1])) for line in data]
+v =max(abs(lit) for clause in clauses for lit in clause)
 
-for assign in itertools.product([True,False],repeat=v):
-    if all(any(assign[abs(lit)-1]== (lit > 0) for lit in clause) for clause in clauses):
+for assign in range(1 << v):
+    if all(any(((assign>>(abs(lit)-1))&1) == (lit > 0) for lit in clause) for clause in clauses):
         print("satisfiable")
         sys.exit()
 
